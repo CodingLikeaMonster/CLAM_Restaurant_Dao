b5cdfc77cd29a59e2c3f43d1385322299a664cb6
Gorka <codinglikeamonster@gmail.com>
2020-11-05 17:27:04 +0100

    Creacion del proyecto

---
+++ CLAM_Restaurant_Dao/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAM_Restaurant_Dao
+{
+    public class Class1
+    {
+    }
+}
